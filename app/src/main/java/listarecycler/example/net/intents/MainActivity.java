a8d5f8de6634735ce9bba7cbfe935088a586d153
arnow_000 <arnowelter03@gmail.com>
2016-11-22 16:17:57 +0100

    práctica_3_27

--- app/src/main/java/listarecycler/example/net/intents/MainActivity.java
+++ app/src/main/java/listarecycler/example/net/intents/MainActivity.java
@@ -1,6 +1,7 @@
 package listarecycler.example.net.intents;
 
 import android.Manifest;
+import android.app.SearchManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -12,7 +13,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button web,tlf,maps,fot,corr;
+    Button web,tlf,maps,fot,corr,busc,dial,streetView,comp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +25,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         maps = (Button)findViewById(R.id.Maps);
         fot = (Button)findViewById(R.id.Foto);
         corr = (Button)findViewById(R.id.Correo);
+        busc = (Button)findViewById(R.id.busca);
+        dial = (Button)findViewById(R.id.dialer);
+        streetView = (Button)findViewById(R.id.street);
+        comp = (Button)findViewById(R.id.compartir);
 
         web.setOnClickListener(this);
         tlf.setOnClickListener(this);
         maps.setOnClickListener(this);
         fot.setOnClickListener(this);
         corr.setOnClickListener(this);
+        busc.setOnClickListener(this);
+        dial.setOnClickListener(this);
+        streetView.setOnClickListener(this);
+        comp.setOnClickListener(this);
     }
 
-    public void pgWeb(View view) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.AbrirWeb:
+                pgWeb();
+                break;
+
+            case R.id.LlamarTelefono:
+                llamadaTelefono();
+                break;
+
+            case R.id.Maps:
+                googleMaps();
+                break;
+
+            case R.id.Foto:
+                hacerFoto();
+                break;
+
+            case R.id.Correo:
+                mandarCorreo();
+                break;
+
+            case R.id.busca:
+                busquedaWeb();
+                break;
+
+            case R.id.dialer:
+                dialerTelefono();
+                break;
+
+            case R.id.street:
+                streetView();
+                break;
+
+            case R.id.compartir:
+                share();
+                break;
+        }
+    }
+
+    public void pgWeb() {
         Intent intent = new Intent(Intent.ACTION_VIEW,
                 Uri.parse("http://campus.somtic.net/"));
         startActivity(intent);
     }
-    public void llamadaTelefono(View view) {
+    public void llamadaTelefono() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (this.checkSelfPermission(
                     Manifest.permission.CALL_PHONE) ==
@@ -53,16 +103,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }
     }
-    public void googleMaps(View view) {
+    public void googleMaps() {
         Intent intent = new Intent(Intent.ACTION_VIEW,
                 Uri.parse("geo:38.553468,-0.121579"));
         startActivity(intent);
     }
-    public void hacerFoto(View view) {
+    public void hacerFoto() {
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         startActivity(intent);
     }
-    public void mandarCorreo(View view) {
+    public void mandarCorreo() {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_SUBJECT, "asunto");
@@ -71,29 +121,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new String[] {"smira@iesperemaria.com" });
         startActivity(intent);
     }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.AbrirWeb:
-                pgWeb(v);
-            break;
-
-            case R.id.LlamarTelefono:
-                llamadaTelefono(v);
-            break;
-
-            case R.id.Maps:
-                googleMaps(v);
-            break;
-
-            case R.id.Foto:
-                hacerFoto(v);
-            break;
-
-            case R.id.Correo:
-                mandarCorreo(v);
-            break;
-        }
+    private void busquedaWeb(){
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        intent.putExtra(SearchManager.QUERY,"IES Pere Maria Orts");
+        startActivity(intent);
     }
+
+    private void dialerTelefono()
+    {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:966870700"));
+        startActivity(intent);
+    }
+
+    private void streetView()
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("google.streetview:cbll=38.553468,0.121579"));
+        startActivity(intent);
+    }
+
+    private void share()
+    {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, "Compartido desde IES Pere Maria Orts.");
+        intent.setType("text/plain");
+        startActivity(Intent.createChooser(intent, getResources().getText(R.string.share)));
+    }
+
 }
